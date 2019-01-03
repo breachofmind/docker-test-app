46052c3d21199b9b391383967ac7f6ed80ea0052
Mike Adamczyk <mike.adamczyk@nike.com>
2019-01-03 14:46:18 -0800

    Make right

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -9,7 +9,7 @@ describe('HelloWorld.vue', () => {
     });
     expect(wrapper.text()).toMatch(msg);
   });
-  it('should fail', () => {
-    expect(true).toBe(false);
+  it('should not fail', () => {
+    expect(true).toBe(true);
   });
 });
